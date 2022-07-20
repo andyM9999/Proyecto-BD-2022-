b9c2214eb25b5560573ba1a6b54234891f16ad0d
Andrea Mancilla <72624191+andyM9999@users.noreply.github.com>
2022-07-19 22:59:11 -0400

    Update insert_manual.py

--- insert_manual.py
+++ insert_manual.py
@@ -1,1 +1,29 @@
+import mysql.connector as mariadb
 
+#conecta base de datos
+conn = mariadb.connect(
+        user="root",
+        password="root",
+        host="localhost",
+        port=3306
+    )
+
+#crea cursor
+cur = conn.cursor()
+
+#recibe respuesta
+url = input("Introduzca url: ")
+texto = input("Introduzca título: ")
+
+respuesta= (url,texto)
+
+sql= "Insert into web_scrapping(url, title) values(?,?)"
+
+#inserta datos a la tabla
+if (cur.execute(sql,respuesta)):
+    print("Ingresado con exito")
+else: print("Ha ocurrido un error")
+
+
+cur.commint()
+cur.close()

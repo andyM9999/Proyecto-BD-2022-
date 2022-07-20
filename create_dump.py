c6951c209fee3978f71a116e05c2839b2852609a
Andrea Mancilla <72624191+andyM9999@users.noreply.github.com>
2022-07-19 23:20:08 -0400

    Update create_dump.py

--- create_dump.py
+++ create_dump.py
@@ -1,1 +1,23 @@
+import configparser
+import os
+import time
+import getpass
 
+HOST='localhost'
+PORT='3306'
+DB_USER='root'
+DB_PASS='root'
+
+databases=('web_scrapping')
+
+def get_dump(database):
+    filestamp = time.strftime('%Y-%m-%d-%I')
+    
+    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (HOST,PORT,DB_USER,DB_PASS,database,database+"_"+filestamp))
+    
+    print("\n|| Database dumped to "+database+"_"+filestamp+".sql || ")
+
+
+if __name__=="__main__":
+    for database in databases:
+        get_dump(database)
